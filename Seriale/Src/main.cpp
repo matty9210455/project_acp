c8d575f7c8ade2125b371a42f618960dd990cc86
matty9210455 <mattia23091992>
2015-12-29 13:43:56 +0100

    aggiunti controlli do i nomi dei file di input e output

--- Seriale/Src/main.cpp
+++ Seriale/Src/main.cpp
@@ -20,6 +20,15 @@ int main (int argc, char* argv[]){
     int value;
 
     //apertura e controllo file
+    if(argc<3){	
+		if(argc<2){
+    		cout<<"non ho nome file input"<<endl;
+    		return 0;
+		}
+		cout<<"non ho nome file output"<<endl;
+		//return 0;	
+	}	
+	
     ifstream f(argv[1]);
     string s;
     if(!f){//check file giusto
@@ -32,12 +41,13 @@ int main (int argc, char* argv[]){
     for(auto it=s.begin();it<s.end();it++){//iterazioni
             value=atoi(&(*it));
             iteration.push_back(value);
-            it++;
+            while((*it)!=','&& it<s.end()) it++;
     }
     f.close();
 
         Matrix A(argv[1]);
-        A.print(argv[2],1,0);
+    
+	    A.print(argv[2],1,0);
     auto N=iteration.size();
     for (unsigned int i=0;i<N;i++){
     A.update(iteration[i]);
